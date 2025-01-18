7a6d4c2ed26c8304c0b6b7e6c78236bc22516f4a
Pouria Dana <pouriadana73@gmail.com>
2025-01-18 09:52:38 +0330

    basic list functionality

---
+++ linked_list (OOP)/slink_oop.h
@@ -0,0 +1,40 @@
+#ifndef SLNK_OOP
+#define SLNK_OOP
+
+template<typename T>
+class List {
+    public:
+        List(T val);
+        void add(T val);
+        typename List<T>::Node *get() const;
+    private:
+        struct Node {
+            T val;
+            Node *next;
+        };
+        Node *head = nullptr;
+};
+
+template<typename T>
+List<T>::List(T val) 
+{
+    head = new Node{val, nullptr};
+}
+
+template<typename T>
+void List<T>::add(T val) {
+    Node *new_node = new Node{val, nullptr}; 
+    Node *t = head;
+    while (t->next != nullptr) {
+        t = t->next;
+    }
+    t->next = new_node;
+}
+
+template<typename T>
+typename List<T>::Node *List<T>::get() const 
+{
+    return head;
+}
+
+#endif 
